e55927fc093ebfea7dd3703e7f8c28b450db131b
Almaz <sennaarer@yahoo.com>
2014-01-28 02:08:34 +0600

    Worked with less: selected some color for site

--- library/js/app.js
+++ library/js/app.js
@@ -1,2 +1,19 @@
 var app = angular.module('sennaarer.github.io', []);
 
+/*
+Advent Pro
+Titillium Web
+Raleway
+Roboto Condensed
+Archivo Narrow
+Oswald
+Antic
+Questrial
+Economica
+Michroma
+Orbitron
+Marvel
+Metrophobic
+Play
+
+ */
