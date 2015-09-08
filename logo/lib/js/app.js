0c989da4a7669da45f8709f0e8a534ff1baaa1e3
Almaz Mamadaliev <sennaarer@yahoo.com>
2015-09-09 02:45:53 +0600

    Big part

--- logo/lib/js/app.js
+++ logo/lib/js/app.js
@@ -20,18 +20,8 @@
         $('#toggling').addClass('hidden').empty();
     });
     $(document).on('click','#gallery_more',function(){
-        if ($(this).hasClass('less')){
-            $(this).removeClass('less');
-            $('.design-gallery-right').removeClass('hidden-xs');
-            $(this).text('LESS');
-        }
-        else {
-            $(this).addClass('less')
-            $('.design-gallery-right').addClass('hidden-xs');
-            $(this).text('MORE');
-
-        }
-
+        $('.design-gallery-right').removeClass('hidden-xs');
+        $(this).addClass('disabled');
     });
 }(jQuery);
 
