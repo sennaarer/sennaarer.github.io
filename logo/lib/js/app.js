dbc208444b066380bf1791076aae252a2ac0dfcd
Almaz Mamadaliev <sennaarer@yahoo.com>
2015-09-09 01:08:49 +0600

    Big part

--- logo/lib/js/app.js
+++ logo/lib/js/app.js
@@ -1,8 +1,19 @@
 +function ($) {
     'use strict';
-    $(document)
-        .on('click', '.icon-three-bars', function () {
-            console.log('threeeee....')
-        })
+    $(document).on('click', '.icon-three-bars', function () {
+        var menu = $('.menu').html();
+        var phone = $('.phone').html();
+        if ($(this).hasClass('opened')) {
+            $(this).removeClass('opened');
+            $('#toggling').addClass('hidden').empty();
+            console.log('closed');
+        }
+        else {
+            $(this).addClass('opened');
+            $('#toggling').removeClass('hidden').append(menu).append(phone);
+            console.log('opened')
+        }
+
+    });
 }(jQuery);
 
